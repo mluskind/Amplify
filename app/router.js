d1236c811de25df84b92f38138d27efb99d7a8f0
Michael Luskind <michaelluskind@Michaels-MacBook-Pro-2.local>
2016-09-26 19:02:05 +0100

    first commit

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('carriers');
+  this.route('shippers');
+  this.route('resources');
+  this.route('about');
 });
 
 export default Router;
